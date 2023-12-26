e0c9f9ac0246b3483a652bc30ed58c9eb4ebb8b1
BibekTheLegend <53642289+BibekGupta@users.noreply.github.com>
2023-12-26 01:55:20 -0800

    Add files via upload

---
+++ Text To Speech/background.js
@@ -0,0 +1,43 @@
+Debug_Mode = true;
+
+Debug_Mode ? console.log("[🟢] bg.js is running...") : null;
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: "Read Loud ",
+    contexts: ["selection"],
+    id: "readLoudContextMenu"
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === "readLoudContextMenu") {
+    // Filter out unwanted characters
+    const filteredText = filterText(info.selectionText);
+
+    Debug_Mode ? console.log("Before:", info.selectionText) : null ;
+
+    // Read the filtered text
+    const utterance = new SpeechSynthesisUtterance(filteredText);
+
+    Debug_Mode ? console.log("After:", filteredText) : null ;
+
+    window.speechSynthesis.speak(utterance);
+  }
+});
+
+function filterText(text) {
+  // Define the characters you want to filter out
+  const unwantedChars = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/g;
+
+  // Remove unwanted characters from the text
+  const filteredText = text.replace(unwantedChars,'');
+
+  return filteredText;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'openSettings') {
+    chrome.runtime.openOptionsPage();
+  }
+});
---
+++ Text To Speech/popup.js
@@ -0,0 +1,5 @@
+document.getElementById('stopButton').addEventListener('click', function () {
+    // Stop the speech synthesis
+    window.speechSynthesis.cancel();
+  });
+
