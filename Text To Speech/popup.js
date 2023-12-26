49d9d407eb071047bba359c6b16d462feb0e7ade
BibekTheLegend <53642289+BibekGupta@users.noreply.github.com>
2023-12-26 04:06:03 -0800

    Update popup.js
    Added All button fuctions

--- Text To Speech/popup.js
+++ Text To Speech/popup.js
@@ -1,5 +1,30 @@
-document.getElementById('stopButton').addEventListener('click', function () {
-    // Stop the speech synthesis
-    window.speechSynthesis.cancel();
-  });
+let utterance;
+let paused = false;
 
+document.getElementById('stopButton').addEventListener('click', function () {
+  window.speechSynthesis.cancel();
+  document.getElementById('status').innerText = '[ Stopped ]';
+  utterance = null; // Reset utterance
+  paused = false;
+});
+
+document.getElementById('pauseButton').addEventListener('click', function () {
+  window.speechSynthesis.pause();
+  document.getElementById('status').innerText = '[ Pause ]';
+  paused = true;
+});
+
+document.getElementById('resumeButton').addEventListener('click', function () {
+  if (paused && utterance) {
+    window.speechSynthesis.resume();
+    document.getElementById('status').innerText = '[ Resumed ]';
+  } else {
+    // Check if utterance is already set; if not, create a new one
+    if (!utterance) {
+      utterance = new SpeechSynthesisUtterance(utterance);
+    }
+    window.speechSynthesis.speak(utterance);
+    document.getElementById('status').innerText = '[ Nothing To Resume ]';
+  }
+  paused = false;
+});
